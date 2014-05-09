de1609184d08c5a519fcc71872b0390097978f9e
thomaslauer <thomaslauer@users.noreply.github.com>
2014-05-09 13:19:19 -0500

    Add GL_CULL_FACE
    hopeful performance increases!

--- src/com/base/game/render/Window.java
+++ src/com/base/game/render/Window.java
@@ -34,6 +34,7 @@ public class Window {
 		glClearColor(0.2f, 0.2f, 0.9f, 1f);
 		glEnable(GL_DEPTH_TEST);
 		glEnable(GL_TEXTURE_2D);
+		glEnable(GL_CULL_FACE)
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
