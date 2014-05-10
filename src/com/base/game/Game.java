dec06ce4505fda872cca617acce3e9d46f663b29
Thomas Lauer <thomaslauer01@gmail.com>
2014-05-10 18:35:46 -0500

    Added a test Mesh and with Vertex Array Buffers

--- src/com/base/game/Game.java
+++ src/com/base/game/Game.java
@@ -3,8 +3,11 @@ package com.base.game;
 import static org.lwjgl.opengl.GL11.*;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import com.base.game.render.Mesh;
 import com.base.game.render.TextureMap;
+import com.base.game.render.Vertex;
 import com.base.game.render.Window;
 import com.base.game.update.UpdateJob;
 
@@ -13,11 +16,24 @@ public class Game {
 	
 	public static World world;
 	
+	private Mesh mesh;
 	
 	public Game(int width, int height, String title){
 		world = new World();
 		Window.initWindow(width, height, title);
 		Window.initProjection(70, (float)Window.getWidth()/(float)Window.getHeight(), 0.1f, 1000);
+		
+		//start mesh testing
+		mesh = new Mesh();
+		
+		Vertex[] data = new Vertex[] {new Vertex(new Vector3f(-1, -1, 0)),
+									  new Vertex(new Vector3f(0, 1, 0)),
+									  new Vertex(new Vector3f(1, -1, 0))};
+		
+		mesh.addVertices(data);
+		//end mesh testing
+		
+		
 		TextureMap.init("TextureMap.png");
 	}
 	
@@ -73,6 +89,7 @@ public class Game {
 		glLoadIdentity();
 		world.getPlayer().getCamera().useView();
 		world.render();
+		mesh.draw();
 		Window.update();
 	}
 }
--- src/com/base/game/Utils.java
+++ src/com/base/game/Utils.java
@@ -1,11 +1,15 @@
 package com.base.game;
 
 import java.io.IOException;
+import java.nio.FloatBuffer;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.util.vector.Vector2f;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.util.ResourceLoader;
+
+import com.base.game.render.Vertex;
 
 public class Utils {
 	public static Texture loadTexture(String fileType, String fileName){
@@ -22,5 +26,26 @@ public class Utils {
 		float x = a.x - b.x;
 		float y = a.y - b.y;
 		return new Vector2f(x, y);
+	}
+	
+	public static FloatBuffer createFloatBuffer(int size)
+	{
+		return BufferUtils.createFloatBuffer(size);
+	}
+
+	public static FloatBuffer createFlippedBuffer(Vertex[] vertices)
+	{
+		FloatBuffer buffer = createFloatBuffer(vertices.length * Vertex.SIZE);
+
+		for(int i = 0; i < vertices.length; i++)
+		{
+			buffer.put(vertices[i].getPos().getX());
+			buffer.put(vertices[i].getPos().getY());
+			buffer.put(vertices[i].getPos().getZ());
+		}
+
+		buffer.flip();
+
+		return buffer;
 	}
 }
